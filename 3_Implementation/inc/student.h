e797bb45daa10b0e7341795a925220c491dae217
AdithyaaRavishankar <86227942+AdithyaaRavishankar@users.noreply.github.com>
2022-03-30 11:10:31 +0530

    Create student.h

---
+++ 3_Implementation/inc/student.h
@@ -0,0 +1,73 @@
+/**
+
+* @file student.h 
+
+* @author AdithyaaRavishankar (E book on C@domain.com)
+
+* @brief this file contains all the functions and header files that are used in the main c file. 
+
+*  @version 0.1
+
+*  @date 2022-03-30
+
+*    
+
+*  @copyright Copyright (c) 2022
+
+*    
+
+* 
+
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+void SetColor(int ForgC) 
+  /**
+@brief  This will set the forground color for printing in a console window.
+*
+*/
+
+void ClearConsoleToColors(int ForgC, int BackC)
+ /** 
+  @ brief This is used for clearing the console for the colors
+  
+ *
+ */
+ 
+void gotoxy(int x, int y)
+
+  
+void clearWindow()
+  
+void window() 
+  
+void get_password(char* pass)
+  
+
+void use_pass_field()
+  
+  
+void print_heading(const char st[])
+  
+  
+void add_student()
+  
+  
+  void search_student()
+  
+  void mod_student()
+  
+  void delete_student()
+  
+  void main_window()
+  
+  
+ 
+  
+
