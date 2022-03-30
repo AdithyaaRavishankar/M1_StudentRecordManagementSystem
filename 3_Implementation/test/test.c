91e41a9523f76e38d1fac4e8b44b22b23bff73cf
AdithyaaRavishankar <86227942+AdithyaaRavishankar@users.noreply.github.com>
2022-03-30 11:45:49 +0530

    Update test.c

--- 3_Implementation/test/test.c
+++ 3_Implementation/test/test.c
@@ -19,6 +19,24 @@ void test_search_student(void)
   TEST_ASSERT_EQUAL(1,search_student())
 
 }
+void test_mod_student(void)
+{
+  
+  int studentid = 1;
+  TEST_ASSERT_EQUAL(1,mod_student())
+
+
+
+}
+void test_delete_student(void)
+{
+  
+  int studentid = 1;
+  TEST_ASSERT_EQUAL(1,delete_student())
+
+
+
+}
 
 
 
@@ -26,9 +44,20 @@ void test_search_student(void)
 
 int main(void)
 {
+  
+  /* Initiate the Unity Test Framework */
+
+  
   UNITY_BEGIN();
   
-  RUN_TEST();
+   /* Run Test functions */
+  
+  RUN_TEST(test_search_student);
+  RUN_TEST(test_mod_student);
+  RUN_TEST(test_delete_student);
+  
+  /* Close the Unity Test Framework */
+  
   return UNITY_END();
 
 
