878ef45151dab17e19f6787cb78faffb2e0d3b05
AdithyaaRavishankar <86227942+AdithyaaRavishankar@users.noreply.github.com>
2022-04-01 16:52:16 +0530

    Create welcome.c

---
+++ 3_Implementation/src/welcome.c
@@ -0,0 +1,25 @@
+#include "Student_Management.h"
+//Head message
+void headMessage(const char *message)
+{
+    system("clear");
+    printf("\n\t\t\t---------------------------------------------------------------------------\n");
+    printf("\n\t\t\t\t\tStudent Record Management System Project in C\n");
+    printf("\n\t\t\t----------------------------------------------------------------------------");
+}
+//Display message
+void welcomeMessage()
+{
+    printf("\n\n\n\n\n");
+    printf("\n\t\t\t  **-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**\n");
+    printf("\n\t\t\t        =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+    printf("\n\t\t\t        =                  WELCOME                  =");
+    printf("\n\t\t\t        =                    TO                     =");
+    printf("\n\t\t\t        =               Student Record              =");
+    printf("\n\t\t\t        =                 MANAGEMENT                =");
+    printf("\n\t\t\t        =                   SYSTEM                  =");
+    printf("\n\t\t\t        =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+    printf("\n\t\t\t  **-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**\n");
+    printf("\n\n\n\t\t\t Enter any key to continue.....");
+    getchar();
+}
