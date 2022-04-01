81ca914a287cc9f5258f6268c8f9b7eeb3bd4b39
AdithyaaRavishankar <86227942+AdithyaaRavishankar@users.noreply.github.com>
2022-04-01 15:33:37 +0530

    Update MyProject.c

--- 3_Implementation/MyProject.c
+++ 3_Implementation/MyProject.c
@@ -1,218 +1,39 @@
 #include <stdio.h>
-#include <string.h>
-#include <conio.h>
 #include <stdlib.h>
-#include <windows.h>
-struct student{
-    char SID[15];
-    char name[20];
-    char add[20];
-    char parname[20];
-    int Class;
-    long unsigned int phone_no;
-};
-struct student stu;
-void SetColor(int ForgC)
+#include <time.h>
+#include <string.h>
+#include <ctype.h>
+extern void headMessage(const char *message);
+extern void welcomeMessage();
+//Used macro
+#define MAX_YR  9999
+#define MIN_YR  1900
+#define MAX_SIZE_USER_NAME 30
+#define MAX_SIZE_PASSWORD  20
+#define FILE_NAME  "studentRecordSystem.bin"
+// Macro related to the students info
+#define MAX_FATHER_NAME 50
+#define MAX_STUDENT_NAME 50
+#define MAX_STUDENT_ADDRESS 300
+#define FILE_HEADER_SIZE  sizeof(sFileHeader)
+//structure to store date
+typedef struct
 {
-     WORD wColor;
-     HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-     CONSOLE_SCREEN_BUFFER_INFO csbi;
-     if(GetConsoleScreenBufferInfo(hStdOut, &csbi))
-     {
-          wColor = (csbi.wAttributes & 0xF0) + (ForgC & 0x0F);
-          SetConsoleTextAttribute(hStdOut, wColor);
-     }
-     return;
-}
-void ClearConsoleToColors(int ForgC, int BackC)
+    int yyyy;
+    int mm;
+    int dd;
+} Date;
+typedef struct
 {
-     WORD wColor = ((BackC & 0x0F) << 4) + (ForgC & 0x0F);
-     HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-     COORD coord = {0, 0};
-     DWORD count;
-     CONSOLE_SCREEN_BUFFER_INFO csbi;
-     SetConsoleTextAttribute(hStdOut, wColor);
-     if(GetConsoleScreenBufferInfo(hStdOut, &csbi))
-     {
-          FillConsoleOutputCharacter(hStdOut, (TCHAR) 32, csbi.dwSize.X * csbi.dwSize.Y, coord, &count);
-          FillConsoleOutputAttribute(hStdOut, csbi.wAttributes, csbi.dwSize.X * csbi.dwSize.Y, coord, &count );
-          SetConsoleCursorPosition(hStdOut, coord);
-     }
-     return;
-}
-COORD coord = {0,0}; ///set the cordinate to 0, 0 (top-left corner of window);
-void gotoxy(int x, int y){
-    coord.X = x; coord.Y = y; /// X and Y coordinates
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-}
-void clearWindow(){
-    int i,j;
-    for(i = 37; i < 78; i++){
-        for(j = 7; j < 25; j++){
-            gotoxy(i,j);printf(" ");
-        }
-    }
-    return;
-}
-void window(){
-    gotoxy(28,2);
-    SetColor(35);
-    printf("Student Record Management System");
-    SetColor(17);
-}
-void print_heading(const char st[]){
-    gotoxy(45,8);printf("SRMS : %s",st);
-}
-void add_student(){
-    clearWindow();
-    print_heading("Add Record");
-    int print = 37;
-    FILE *fp;
-    fp = fopen("record.txt","ab+");
-    SetColor(45);
-    if(fp == NULL){
-        MessageBox(0,"Error in Opening file\nMake sure your file is not write protected","Warning",0);
-
-    }else{
-        fflush(stdin);
-        gotoxy(print,10);printf("Student ID: ");gets(stu.SID);
-        gotoxy(print,12);printf("Name of the student: ");gets(stu.name);
-        gotoxy(print,14);printf("Residential Address: ");gets(stu.add);
-        gotoxy(print,16);printf("Parent's name: ");gets(stu.parname);
-        gotoxy(print,18);printf("Class: ");scanf("%d",&stu.Class);
-        gotoxy(print,20);printf("Phone Number: ");scanf("%ld",&stu.phone_no);
-        fwrite(&stu, sizeof(stu), 1, fp);
-        gotoxy(40,22); printf("The record is sucessfully added");
-    }
-    SetColor(28);
-    fclose(fp);
-    return;
-}
-
-void search_student(){
-    clearWindow();
-    print_heading("Search Record");
-    SetColor(45);
-    char s_id[15];
-    int isFound = 0;
-    gotoxy(37,10);printf("Enter ID to Search: ");fflush(stdin);
-    gets(s_id);
-    FILE *fp;
-    fp = fopen("record.txt","rb");
-    while(fread(&stu,sizeof(stu),1,fp) == 1){
-        if(strcmp(s_id,stu.SID) == 0){
-            isFound = 1;
-            break;
-        }
-    }
-    if(isFound == 1){
-        gotoxy(37,12);printf("The record is Found");
-        gotoxy(37,14);printf("ID: %s",stu.SID);
-        gotoxy(37,15);printf("Name: %s",stu.name);
-        gotoxy(37,16);printf("Address: %s",stu.add);
-        gotoxy(37,17);printf("Parent's Name: %s",stu.parname);
-        gotoxy(37,18);printf("Class: %d",stu.Class);
-        gotoxy(37,19);printf("Phone No: %ld",stu.phone_no);
-    }else{
-        gotoxy(37,12);printf("Sory, No record found in the database");
-    }
-    SetColor(28);
-    fclose(fp);
-    return;
-}
-void mod_student(){
-    clearWindow();
-    print_heading("Modify Record");
-    SetColor(45);
-    char s_id[15];
-    int isFound = 0, print = 37;
-    gotoxy(37,10);printf("Enter ID to Modify: ");fflush(stdin);
-    gets(s_id);
-    FILE *fp;
-    fp = fopen("record.txt","rb+");
-    while(fread(&stu, sizeof(stu),1,fp) == 1){
-        if(strcmp(s_id, stu.SID) == 0){
-            fflush(stdin);
-            gotoxy(print,12);printf("Student ID: ");gets(stu.SID);
-            gotoxy(print,13);printf("Name of the student: ");gets(stu.name);
-            gotoxy(print,14);printf("Residential Address: ");gets(stu.add);
-            gotoxy(print,15);printf("Parent's name: ");gets(stu.parname);
-            gotoxy(print,16);printf("Class: ");scanf("%d",&stu.Class);
-            gotoxy(print,17);printf("Phone Number: ");scanf("%ld",&stu.phone_no);
-            fseek(fp,-sizeof(stu), SEEK_CUR);
-            fwrite(&stu,sizeof(stu), 1, fp);
-            isFound = 1;
-            break;
-        }
-    }
-    if(!isFound){
-        gotoxy(print, 12);printf("No Record Found");
-    }
-    fclose(fp);
-    SetColor(28);
-    return;
-}
-void delete_student(){
-    clearWindow();
-    print_heading("Delete Record");
-    SetColor(45);
-    char s_id[15];
-    int isFound = 0, print = 37;
-    gotoxy(37,10);printf("Enter ID to Modify: ");fflush(stdin);
-    gets(s_id);
-    FILE *fp, *temp;
-    fp = fopen("record.txt","rb");
-    temp = fopen("temp.txt", "wb");
-    while(fread(&stu, sizeof(stu),1,fp) == 1){
-        if(strcmp(s_id, stu.SID) == 0){
-            fwrite(&stu,sizeof(stu),1,temp);
-        }
-    }
-    fclose(fp);
-    fclose(temp);
-    remove("record.txt");
-    rename("temp.txt","record.txt");
-    gotoxy(37,12);printf("The record is sucessfully deleted");
-    SetColor(28);
-    return;
-}
-void main_window(){
-    int choice;
-    SetColor(28);
-    int x = 2;
-    while(1){
-        gotoxy(x,8);printf("1. Add Student");
-        gotoxy(x,10);printf("2. Search Student");
-        gotoxy(x,12);printf("3. Modify Student Record");
-        gotoxy(x,14);printf("4. Delete Student Record");
-        gotoxy(x,16);printf("5. Exit");
-        gotoxy(x,18);printf("Enter your choice: ");
-        scanf("%d",&choice);
-        switch(choice){
-            case 1:
-                add_student();
-                break;
-            case 2:
-                search_student();
-                break;
-            case 3:
-                mod_student();
-                break;
-            case 4:
-                delete_student();
-                break;
-            case 5:
-                exit(0);
-                break;
-            default:
-                break;
-        }
-    }
-}
-int main(){
-    ClearConsoleToColors(17,15);
-    SetConsoleTitle("Student Record Management System");
-    window();
-    main_window();
-    return 0;
-} 
+    char username[MAX_SIZE_USER_NAME];
+    char password[MAX_SIZE_PASSWORD];
+} sFileHeader;
+//Elements of structure
+typedef struct// to call in program
+{
+    unsigned int student_id; // declare the integer data type
+    char fatherName[MAX_FATHER_NAME];// declare the charecter data type
+    char studentName[MAX_STUDENT_NAME];// declare the character data type
+    char studentAddr[MAX_STUDENT_ADDRESS];// declare the character data type
+    Date studentJoiningDate;// declare the integer data type
+} s_StudentInfo;
