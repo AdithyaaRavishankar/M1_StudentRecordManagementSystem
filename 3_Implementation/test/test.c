61b7d6a9baa08b37db704d2a73040def2601226b
AdithyaaRavishankar <86227942+AdithyaaRavishankar@users.noreply.github.com>
2022-04-02 20:24:12 +0530

    Update test.c

--- 3_Implementation/test/test.c
+++ 3_Implementation/test/test.c
@@ -1,64 +1,16 @@
-#include <stdio.h>
-#include "../inc/student.h"
-#include "../unity/unity.h"
+#include "unity.h"
+extern void test_searchStudent();
 
-/* Required by the unity test framework */
 void setUp()
 {
 }
-/* Required by the unity test framework */
 void tearDown()
-{
-
-
+{ 
 }
-void test_search_student(void)
-{
-  
-  int studentid = 1;
-  TEST_ASSERT_EQUAL(1,search_student())
-
-}
-void test_mod_student(void)
-{
-  
-  int studentid = 1;
-  TEST_ASSERT_EQUAL(1,mod_student())
-
-
-
-}
-void test_delete_student(void)
-{
-  
-  int studentid = 1;
-  TEST_ASSERT_EQUAL(1,delete_student())
-
-
-
-}
-
-
-
-
 
 int main(void)
 {
-  
-  /* Initiate the Unity Test Framework */
-
-  
-  UNITY_BEGIN();
-  
-   /* Run Test functions */
-  
-  RUN_TEST(test_search_student);
-  RUN_TEST(test_mod_student);
-  RUN_TEST(test_delete_student);
-  
-  /* Close the Unity Test Framework */
-  
+  UnityBegin(NULL);
+  RUN_TEST(test_searchStudent);
   return UNITY_END();
-
-
 }
